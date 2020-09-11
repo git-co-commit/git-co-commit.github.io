51a0074772323c23b49be00dfa65b76e7d5171d2
GokulnathP <4gokul2000@gmail.com>
2020-09-11 09:40:56 +0530

    React snapshot package for Prerendering added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
